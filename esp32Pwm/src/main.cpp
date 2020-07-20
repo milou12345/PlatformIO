47b7517080174cc46cc574b3a70a0c902eef290f
Milan <milan.larsen@haw-hamburg.de>
2020-07-20 19:38:37 +0200

    Aufgeräumt

--- esp32Pwm/src/main.cpp
+++ esp32Pwm/src/main.cpp
@@ -1,12 +1,15 @@
 #include <Arduino.h>
 #include "Filter.h"
-#include </home/milan/Documents/PlatformIO/esp32Pwm/.pio/libdeps/esp32doit-devkit-v1/Arduino-PID-Library/PID_v1/PID_v1.h>
+#include </home/milou/Documents/PlatformIO/esp32Pwm/.pio/libdeps/esp32doit-devkit-v1/Arduino-PID-Library/PID_v1/PID_v1.h>
 
-//
+// Defines
 #define PIN_INPUT 13
 #define SampleTime 0
 #define freq 100000
-//#define PIN_OUTPUT 3
+#define ledChannel 0
+#define resolution 8
+#define pwmPin 15
+
 
 //Define Variables we'll be connecting to
 double Setpoint, Input, Output;
@@ -18,17 +21,6 @@ PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 // Create a new exponential filter with a weight of 5 and an initial value of 0. 
 //ExponentialFilter<long> ADCFilter(5, 0);
-//Pin Assigment
-const int pwmPin = 15;  
-int pwm=0;
-int mappwm=0;
-int sensVal=0;
-int counter=0;
-
-// setting PWM properties
-
-const int ledChannel = 0;
-const int resolution = 8;
 
 void setup(){
   // configure LED PWM functionalitites
@@ -59,14 +51,8 @@ void loop(){
   
   myPID.Compute();
   
-  //Map Pwm Duty cyle to dac value
-   // mappwm=map(pwm,0,100,0,254);
     //Wirte pwm
     ledcWrite(ledChannel, Output);
-    
-    //Serial Output every 100 cyles
-    //sensVal=analogRead(PIN_INPUT);
-
     //ADCFilter.Filter(sensVal);
      
     
